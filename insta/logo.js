7ee55970e91053e3a0d786e906489b0a1e1f916e
sunilsGubbala <154649760+sunilsGubbala@users.noreply.github.com>
2024-01-04 19:25:55 +0530

    first commit

---
+++ insta/logo.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var insta = document.getElementById('insta');
+    insta.addEventListener('animationend', function () {
+        insta.classList.add('installed');
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var inst = document.getElementById('instasss');
+    inst.addEventListener('animationend', function () {
+        instasss.classList.add('installedss');
+    });
+});
